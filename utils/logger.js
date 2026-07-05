169a31ab4581bbad4aac3f50009ca40f411909e2
kapadigitalmedia-art <kapadigitalmedia@gmail.com>
2026-07-05 18:19:51 +0530

    Delete utils directory

--- utils/logger.js
+++
@@ -1,9 +0,0 @@
-function ts() {
-  return new Date().toISOString().replace('T', ' ').substring(0, 19);
-}
-
-module.exports = {
-  info: (...args) => console.log(`[${ts()}] [INFO]`, ...args),
-  warn: (...args) => console.warn(`[${ts()}] [WARN]`, ...args),
-  error: (...args) => console.error(`[${ts()}] [ERROR]`, ...args),
-};
